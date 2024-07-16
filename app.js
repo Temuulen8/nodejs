e79299e02a05c0affee195c1457e77e740684793
Temuulen8 <b.temuulen2005@gmail.com>
2024-07-16 12:33:29 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// //ES5 variables
+// var aa = 200;
+// var bb = 300;
+
+// //ES5 function
+// function print(){}
+
+// //ES6 variables
+// const a = 100;
+// const b = 200;
+
+// const c = a + b;
+
+// //ES6 function
+// const printf = () => {};
+
+// console.log("answer:", c);
+
+// //module
+// const add = (a, b)=>{
+// 	return a + b
+// }
+
+// const sub = (a, b)=>{
+// 	return a - b
+// }
+
+// const nemeh  = require("./myMath")
+// const sub  = require("./myMath")
+
+// console.log("add;", nemeh(10, 20))
+
+const person = {
+  name: "Naraa", //property
+  age: 20,
+
+  hevle: function () {},
+};
+// console.log("name:",person.name )
+// console.log("name:",person.age )
+
+//  const name = person.name
+//  const age = person.name
+
+const { name, age } = person;
+
+//array method => map
